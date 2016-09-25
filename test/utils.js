6417f2d66abab8e38ebe0f2c6cf748073bc29e10
Maxime Guedj <max.guedj13@gmail.com>
2016-09-25 13:45:20 +0200

    Add utils function

--- test/utils.js
+++ test/utils.js
@@ -12,3 +12,13 @@ it('uidGen', function () {
   const uid = common.utils.uidGen(fixture.uidGen.len);
   expect(uid).to.have.lengthOf(fixture.uidGen.len);
 });
+
+it('arrayHasDuplicate', function () {
+  const result = common.utils.arrayHasDuplicate(['test', 'tests', 'hello', 'salut', 'test']);
+  expect(result).to.be.true;
+});
+
+it('arrayHasDuplicate', function () {
+  const result = common.utils.arrayHasDuplicate(['test', 'tests', 'hello', 'salut']);
+  expect(result).to.be.false;
+});
