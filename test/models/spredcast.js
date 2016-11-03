7e750f67fe1b56bfc7e8991ff383a3aae20d3e69
=guedj_m <maxime.guedj@epitech.eu>
2016-11-03 17:01:39 +0100

    Integrate cast models

--- test/models/spredcast.js
+++ test/models/spredcast.js
@@ -8,18 +8,19 @@ var user2;
 var user3;
 var cast1;
 var cast2;
+var token;
 
 describe('Testing Spredcast models', function () {
   before(function (done) {
-    common.userModel.createPassword(fixture.user.email, fixture.user.password, fixture.user.pseudo, "", "", function (err, cUser) {
+    common.userModel.createPassword(fixture.user.email, fixture.user.password, fixture.user.pseudo, '', '', function (err, cUser) {
       if (err) {
         done(err);
       } else {
-        common.userModel.createPassword(fixture.user2.email, fixture.user2.password, fixture.user2.pseudo, "", "", function (err, cUser2) {
+        common.userModel.createPassword(fixture.user2.email, fixture.user2.password, fixture.user2.pseudo, '', '', function (err, cUser2) {
           if (err) {
             done(err);
           } else {
-            common.userModel.createPassword(fixture.user3.email, fixture.user3.password, fixture.user3.pseudo, "", "", function (err, cUser3) {
+            common.userModel.createPassword(fixture.user3.email, fixture.user3.password, fixture.user3.pseudo, '', '', function (err, cUser3) {
               if (err) {
                 done(err);
               } else {
@@ -107,6 +108,153 @@ describe('Testing Spredcast models', function () {
         } else {
           expect(authorisation).to.be.false;
           done();
+        }
+      });
+    });
+  });
+
+  describe('spredCastModel.updateState()', function () {
+    it('Should update state of the spredCast', function (done) {
+      common.spredCastModel.updateState(cast1._id, 1, function (err) {
+        if (err) {
+          done(err);
+        } else {
+          common.spredCastModel.getById(cast1._id, function (err, fCast) {
+            if (err) {
+              done(err);
+            } else {
+              expect(fCast.state).to.equal(1);
+              done();
+            }
+          });
+        }
+      });
+    });
+
+    it('Should update state of the spredCast', function (done) {
+      common.spredCastModel.updateState(cast2._id, 1, function (err) {
+        if (err) {
+          done(err);
+        } else {
+          common.spredCastModel.getById(cast2._id, function (err, fCast) {
+            if (err) {
+              done(err);
+            } else {
+              expect(fCast.state).to.equal(1);
+              done();
+            }
+          });
+        }
+      });
+    });
+
+    it('Sould now allow user2 to access cast1', function (done) {
+      common.spredCastModel.userCanJoin(cast1._id, user2._id, function (err, authorisation, fCast) {
+        if (err) {
+          done(err);
+        } else {
+          expect(authorisation).to.be.true;
+          done();
+        }
+      });
+    });
+
+    it('Sould now allow user2 to access cast2', function (done) {
+      common.spredCastModel.userCanJoin(cast2._id, user2._id, function (err, authorisation, fCast) {
+        if (err) {
+          done(err);
+        } else {
+          expect(authorisation).to.be.true;
+          done();
+        }
+      });
+    });
+
+    it('Sould not allow user3 to access cast12', function (done) {
+      common.spredCastModel.userCanJoin(cast2._id, user3._id, function (err, authorisation, fCast) {
+        if (err) {
+          done(err);
+        } else {
+          expect(authorisation).to.be.false;
+          done();
+        }
+      });
+    });
+  });
+
+  describe('spredCastModel.updateUserCount()', function () {
+    it('Should update the user count number', function (done) {
+      common.spredCastModel.updateUserCount(cast1._id, 2, function (err) {
+        if (err) {
+          done(err);
+        } else {
+          common.spredCastModel.getById(cast1._id, function (err, fCast) {
+            if (err) {
+              done(err);
+            } else {
+              expect(fCast.userCount).to.equal(2);
+              done();
+            }
+          });
+        }
+      });
+    });
+  });
+
+  describe('castTokenModel.createNew()', function () {
+    it('Should create a new cast token with an existing user', function (done) {
+      common.castTokenModel.createNew(client, user, cast1, true, function (err, cToken) {
+        if (err) {
+          done(err);
+        } else {
+          expect(cToken.pseudo).to.equal(user.pseudo);
+          token = cToken;
+          done();
+        }
+      });
+    });
+
+    it('Should create a new cast token with an anonymous user', function (done) {
+      common.castTokenModel.createNew(client, null, cast1, true, function (err, cToken) {
+        if (err) {
+          done(err);
+        } else {
+          expect(cToken.pseudo).to.not.be.undefined;
+          done();
+        }
+      });
+    });
+  });
+
+  describe('castTokenModel.getByToken()', function () {
+    it('Should find the created token', function (done) {
+      common.castTokenModel.getByToken(token.token, function (err, fToken) {
+        if (err) {
+          done(err);
+        } else {
+          expect(fToken._id.toString()).to.equal(token._id.toString());
+          expect(fToken.user.pseudo).to.equal(user.pseudo);
+          expect(fToken.cast.name).to.equal(cast1.name);
+          done();
+        }
+      });
+    });
+  });
+
+  describe('castTokenModel.revokeCast()', function () {
+    it('Should remove all token generate for cast1', function (done) {
+      common.castTokenModel.revokeCast(cast1._id, function (err) {
+        if (err) {
+          done(err);
+        } else {
+          common.castTokenModel.getByToken(token.token, function (err, fToken) {
+            if (err) {
+              done(err);
+            } else {
+              expect(fToken).to.be.null;
+              done();
+            }
+          });
         }
       });
     });
